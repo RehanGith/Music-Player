d6bc572931466032509bc09a2a2961135b481745
RehanGith <chrehan1007@gmail.com>
2024-12-31 22:39:52 +0700

    Created fragments transaction from mainActivity

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.musicplayer
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.musicplayer.Screens.Musics.MusicFragment
 import com.example.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setTheme(R.style.coolPinkTheme)
+        setFragment(MusicFragment())
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    }
+    private fun setFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentView, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
     }
 }
